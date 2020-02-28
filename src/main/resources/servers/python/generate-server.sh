981231d694039d718461e4d66774a09e47f06b19
Maximiliano Osorio <maxiosorio@gmail.com>
2020-02-27 16:17:10 -0800

    fix: generic generate server

--- src/main/resources/servers/python/generate-server.sh
+++ src/main/resources/servers/python/generate-server.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+set -e
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker is installed"
+else
+    echo "Docker is not installed"
+    exit 1
+fi
+
 dir=${PWD}
 SERVER_DIR=server
 docker run -ti --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v4.1.2 \
@@ -6,13 +14,10 @@ docker run -ti --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v4.1.2 \
      -i /local/model-catalog.yaml\
      -g python-flask  \
      -o /local/$SERVER_DIR/ \
-     --git-repo-id model-catalog-api \
-     --git-user-id mintproject \
      --template-dir /local/.openapi-generator/template \
      --ignore-file-override /local/.openapi-generator-ignore
-rm -f $SERVER_DIR/openapi_server/controllers/default_controller.py
 cp -rv ${PWD}/.openapi-generator/template/static_files/utils/ ${PWD}/$SERVER_DIR/openapi_server/utils/
 cp -rv ${PWD}/.openapi-generator/template/static_files/settings/ ${PWD}/$SERVER_DIR/openapi_server/settings/
 cp -rv ${PWD}/.openapi-generator/template/static_files/user_controller.py ${PWD}/$SERVER_DIR/openapi_server/controllers/
-cp -rv ${PWD}/.openapi-generator/template/static_files/contexts/ ${PWD}/$SERVER_DIR/contexts/
-cp -rv ${PWD}/.openapi-generator/template/static_files/queries/ ${PWD}/$SERVER_DIR/queries/
+mkdir -p ${PWD}/.openapi-generator/template/static_files/contexts/
+mkdir -p ${PWD}/.openapi-generator/template/static_files/queries/
