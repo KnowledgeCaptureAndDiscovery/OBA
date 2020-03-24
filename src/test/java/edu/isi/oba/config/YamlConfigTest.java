e581a72efffaf0a8b911a3a57bb804a97fffa969
Maximiliano Osorio <maxiosorio@gmail.com>
2020-03-24 11:34:01 -0700

    fix: unittest test

--- src/test/java/edu/isi/oba/config/YamlConfigTest.java
+++ src/test/java/edu/isi/oba/config/YamlConfigTest.java
@@ -12,9 +12,9 @@ import static org.junit.Assert.assertThat;
 public class YamlConfigTest {
     @Test
     public void getSelectedClasses(){
-        String config_test_file_path = "examples/dbpedia/config.yaml";
+        String config_test_file_path = "examples/dbpedia/config_music.yaml";
         YamlConfig config_data = get_yaml_data(config_test_file_path);
-        List<String> expected = Arrays.asList("http://dbpedia.org/ontology/Genre", "http://dbpedia.org/ontology/Author");
+        List<String> expected = Arrays.asList("http://dbpedia.org/ontology/Genre", "http://dbpedia.org/ontology/Band");
         List<String> config = config_data.getClasses();
         Assert.assertEquals(expected, config);
     }
