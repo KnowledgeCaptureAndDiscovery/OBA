dde9c402d0cf3b389ec94b79c2aa952ea8b99159
Daniel Garijo <dgarijo@isi.edu>
2020-05-01 20:39:13 -0700

    Fix #98

--- src/test/java/edu/isi/oba/MapperTest.java
+++ src/test/java/edu/isi/oba/MapperTest.java
@@ -14,8 +14,7 @@ import static edu.isi.oba.ObaUtils.get_yaml_data;
 public class MapperTest {
     @Test
     public void testFilter() throws Exception{
-        String example_dbpedia = "src/test/config/dbpedia.yaml";
-        String config_test_file_path = example_dbpedia;
+        String config_test_file_path = "src/test/config/dbpedia.yaml";
         YamlConfig config_data = get_yaml_data(config_test_file_path);
         Mapper mapper = new Mapper(config_data);
         List<String> config = config_data.getClasses();
@@ -28,5 +27,32 @@ public class MapperTest {
         Collections.sort(config);
         Assert.assertEquals(config, filter_classes);
 
+    }
+    
+    /**
+     * This test attempts to load a local ontology.
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testLocalFile() throws Exception{
+        String local_ontology = "src/test/config/mcat_reduced.yaml";
+        YamlConfig config_data = get_yaml_data(local_ontology);
+        Mapper mapper = new Mapper(config_data);
+        Assert.assertEquals(false, mapper.ontologies.isEmpty());
+    }
+    
+    /**
+     * This test attempts to run OBA with an online ontology through a URI.
+     * The ontology is hosted in GitHub, but there is a small risk of the test
+     * not passing due to the unavailability of the ontology.
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testRemoteOntology() throws Exception{
+        String example_remote = "src/test/config/pplan.yaml";
+        YamlConfig config_data = get_yaml_data(example_remote);
+        Mapper mapper = new Mapper(config_data);
+        Assert.assertEquals(false, mapper.ontologies.isEmpty());
+        
     }
 }
