e5972884fc4d591a28a30638b4b980f82745a328
Maximiliano Osorio <mosorio@isi.edu>
2020-09-24 11:54:23 -0700

    fix: wait for the server

--- .ci/dbpedia_music.sh
+++ .ci/dbpedia_music.sh
@@ -7,5 +7,6 @@ pushd server
 docker build -t openapi_server .
 docker run --name dbpedia_music -d -p 8080:8080 openapi_server
 popd
+sleep 10s
 curl -X GET "http://0.0.0.0:8080/v1.3.0/bands/Pink_Floyd" -H  "accept: application/json"
 docker logs dbpedia_music
