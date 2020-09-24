add9c5c339bd2bc848215175d10ba33bd829d788
Maximiliano Osorio <mosorio@isi.edu>
2020-09-24 11:48:52 -0700

    fix: test using pink floyd

--- .ci/dbpedia_music.sh
+++ .ci/dbpedia_music.sh
@@ -5,5 +5,7 @@ pushd outputs/dbpedia_music/servers/python
 bash generate-server.sh
 pushd server
 docker build -t openapi_server .
-docker run -d -p 8080:8080 openapi_server
+docker run --name dbpedia_music -d -p 8080:8080 openapi_server
 popd
+curl -X GET "http://0.0.0.0:8080/v1.3.0/bands/Pink_Floyd" -H  "accept: application/json"
+docker logs dbpedia_music
