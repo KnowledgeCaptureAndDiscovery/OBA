6e20c46691bdf303316da7211d0b52e7d0934bd3
Christopher Weedall <5010253+cweedall@users.noreply.github.com>
2024-06-05 14:01:10 -0500

    add test for PascalCase to kebab-case string conversion

--- src/test/java/edu/isi/oba/ObaUtilsTest.java
+++ src/test/java/edu/isi/oba/ObaUtilsTest.java
@@ -1,7 +1,6 @@
 package edu.isi.oba;
 
 import static edu.isi.oba.ObaUtils.get_yaml_data;
-import edu.isi.oba.config.YamlConfig;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,27 +17,27 @@ public class ObaUtilsTest {
 
     @Test
     public void read_json_file() throws IOException {
-        JSONObject actual = ObaUtils.read_json_file("json_one.json");
+        final var actual = ObaUtils.read_json_file("json_one.json");
         Assertions.assertNotNull(actual.get("@context"));
     }
 
     @Test
     public void mergeJSONObjects() throws IOException {
-        JSONObject one = ObaUtils.read_json_file("json_one.json");
-        JSONObject two = ObaUtils.read_json_file("json_two.json");
-        JSONObject merge = ObaUtils.mergeJSONObjects(one, two);
+        final var one = ObaUtils.read_json_file("json_one.json");
+        final var two = ObaUtils.read_json_file("json_two.json");
+        final var merge = ObaUtils.mergeJSONObjects(one, two);
         Assertions.assertNotNull(merge.get("@context"));
         Assertions.assertNotNull(merge.get("@context"));
     }
 
     @Test
     public void concat_json_common_key() throws IOException {
-        JSONObject one = ObaUtils.read_json_file("json_one.json");
-        JSONObject two = ObaUtils.read_json_file("json_two.json");
-        JSONObject three = ObaUtils.read_json_file("json_three.json");
-        JSONObject[] jsons = new JSONObject[]{ one, two, three};
-        JSONObject merge = ObaUtils.concat_json_common_key(jsons, "@context");
-        JSONObject o = (JSONObject) merge.get("@context");
+        final var one = ObaUtils.read_json_file("json_one.json");
+        final var two = ObaUtils.read_json_file("json_two.json");
+        final var three = ObaUtils.read_json_file("json_three.json");
+        final var jsons = new JSONObject[]{ one, two, three};
+        final var merge = ObaUtils.concat_json_common_key(jsons, "@context");
+        final var o = (JSONObject) merge.get("@context");
         Assertions.assertNotNull(o.get("Entity"));
         Assertions.assertNotNull(o.get("Model"));
         Assertions.assertNotNull(o.get("Setup"));
@@ -46,12 +45,12 @@ public class ObaUtilsTest {
     
     @Test
     public void getDescription () throws OWLOntologyCreationException{
-        String example_remote = "src/test/config/pplan.yaml";
-        YamlConfig config_data = get_yaml_data(example_remote);
+        final var example_remote = "src/test/config/pplan.yaml";
+        final var config_data = get_yaml_data(example_remote);
         try {
-            Mapper mapper = new Mapper(config_data);
-            OWLClass planClass = mapper.manager.getOWLDataFactory().getOWLClass("http://purl.org/net/p-plan#Plan");
-            String desc = ObaUtils.getDescription(planClass, mapper.ontologies.stream().findFirst().get(), true);
+            final var mapper = new Mapper(config_data);
+            OWLClass planClass = mapper.getManager().getOWLDataFactory().getOWLClass("http://purl.org/net/p-plan#Plan");
+            String desc = ObaUtils.getDescription(planClass, mapper.getOntologies(), true);
             Assertions.assertNotEquals(desc, "");
         }catch(Exception e){
             Assertions.fail("Failed to get description.", e);
@@ -65,10 +64,10 @@ public class ObaUtilsTest {
      */
     @Test
     public void missing_file () throws OWLOntologyCreationException{
-        String missing_file = "src/test/config/missing_file.yaml";
-        YamlConfig config_data = get_yaml_data(missing_file);
+        final var missing_file = "src/test/config/missing_file.yaml";
+        final var config_data = get_yaml_data(missing_file);
         try {
-            Mapper mapper = new Mapper(config_data);
+            final var mapper = new Mapper(config_data);
             Assertions.fail("Missing file: If no exception is launched, fail test");
         }catch(Exception e){
             //pass test if there is an exception
@@ -77,13 +76,13 @@ public class ObaUtilsTest {
 
     @Test
     public void run() {
-        String ontology1 = "https://mintproject.github.io/Mint-ModelCatalog-Ontology/release/1.8.0/ontology.owl";
-        String ontology2 = "https://knowledgecaptureanddiscovery.github.io/SoftwareDescriptionOntology/release/1.9.0/ontology.owl";
-        File ont1 = new File("ontology1");
-        File ont2 = new File("ontology2");
+        final var ontology1 = "https://mintproject.github.io/Mint-ModelCatalog-Ontology/release/1.8.0/ontology.owl";
+        final var ontology2 = "https://knowledgecaptureanddiscovery.github.io/SoftwareDescriptionOntology/release/1.9.0/ontology.owl";
+        final var ont1 = new File("ontology1");
+        final var ont2 = new File("ontology2");
         ObaUtils.downloadOntology(ontology1, ont1.getPath());
         ObaUtils.downloadOntology(ontology2, ont2.getPath());
-        String[] ontologies = new String[]{"ontology1", "ontology2"};
+        final var ontologies = new String[]{"ontology1", "ontology2"};
         JSONObject context = null;
         try {
             context = ObaUtils.generate_context_file(ontologies, false);
@@ -91,7 +90,7 @@ public class ObaUtilsTest {
             e.printStackTrace();
         }
 
-        JSONObject o = (JSONObject) context.get("@context");
+        final var o = (JSONObject) context.get("@context");
         Assertions.assertEquals(o.get("id"), "@id");
         Assertions.assertEquals(o.get("type"), "@type");
         Assertions.assertNotNull(o.get("Entity"));
@@ -101,5 +100,34 @@ public class ObaUtilsTest {
             java.nio.file.Files.delete(ont2.toPath());
         }catch(Exception e){
         }
+    }
+
+    /**
+     * This test will try to load a file that does not exits. The exception is captured and reported.
+     * This test will pass IF you see an error on the output terminal
+     * @throws OWLOntologyCreationException
+     */
+    @Test
+    public void testKebabCaseConversion() {
+        // Test #1
+        var expectedStr = "this-is-a-string";
+        var originalStr = "thisIsAString";
+        var convertedStr = ObaUtils.pascalCaseToKebabCase(originalStr);
+
+        Assertions.assertEquals(expectedStr, convertedStr);
+
+        // Test #2
+        expectedStr = "this-happy-string";
+        originalStr = "thisHAPPYString";
+        convertedStr = ObaUtils.pascalCaseToKebabCase(originalStr);
+
+        Assertions.assertEquals(expectedStr, convertedStr);
+
+        // Test #3
+        expectedStr = "this-phd-string";
+        originalStr = "thisPhDString";
+        convertedStr = ObaUtils.pascalCaseToKebabCase(originalStr);
+
+        Assertions.assertEquals(expectedStr, convertedStr);
     }
 }
